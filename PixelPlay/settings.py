604b505693c76f985aad1ca42cf2bc30e5f09bd0
muzkii <andriyo.averill@ui.ac.id>
2024-09-11 11:49:51 +0700

    Ganti PWS

--- PixelPlay/settings.py
+++ PixelPlay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gzvx$$)!i&)+wpgc=j4ai4ps=f%omvii!-4=k7!c@i871wy3j_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andriyo-averill-PixelPlay.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andriyo-averill-pixelplay.pbp.cs.ui.ac.id"]
 
 
 # Application definition
